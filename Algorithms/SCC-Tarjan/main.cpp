d55ec8e73e4507c098b866292736761a5768a805
fedemengo <fede.mengo@hotmail.it>
2018-04-05 19:22:33 +0200

    scc refactor

---
+++ Algorithms/SCC-Tarjan/main.cpp
@@ -0,0 +1,37 @@
+//#include <bits/stdc++.h>
+#include <cmath>
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <map>
+#include <unordered_map>
+#include <set>
+#include <unordered_set>
+#include <queue>
+using namespace std;
+
+typedef pair <int, int> pii;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+typedef vector<vector<int>> graph;
+typedef vector<vector<pii>> wgraph;
+typedef long long ll;
+typedef unsigned long long ull;
+
+const int INF = 1e9;
+const int MOD = 1000000007;
+
+std::vector<int> dfs_num, dfs_low;
+std::vector<bool> visited;
+std::stack<int> S;
+
+int main(int argc, char *argv[]){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    dfs_num.assign()
+
+    return 0;
+}
