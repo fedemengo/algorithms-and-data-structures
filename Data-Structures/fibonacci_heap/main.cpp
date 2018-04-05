8ac5602e2c7b5a931ba58b356cc5af299bedf236
fedemengo <fede.mengo@hotmail.it>
2018-04-05 19:19:31 +0200

    code refactor

--- Data-Structures/fibonacci_heap/main.cpp
+++ Data-Structures/fibonacci_heap/main.cpp
@@ -5,15 +5,15 @@
 #include <iostream>
 
 /**
- *  usage example in Dijkstra's algorithm
- *  the KEY would be the distance to each node, initialized at INF 
- *  the DATA would be an indefier for the node itself
- *  
- *  NOTE: the method fill(size, default_value) and therefore the constructor (size, default_value)
- *  map each node a a INT in the range (0, size)
- *  to avoid that use empty constructor and insert(key, data)
+ *  usage example in SP's algorithm
+ *  the KEY would be the distance to each node, initialized at INF
+ *  the DATA would be an identifier for the node itself
  *
- *  TODO: to speed up the contruction, create SIZE nodes at once and retrieve them from a node "pool"
+ *  NOTE: the method fill(size, default_value) and therefore the constructor (size, default_value)
+ *  map each node to an INT in the range (0, size)
+ *  to avoid that, use empty constructor and insert(key, data)
+ *
+ *  TODO: to speed up the construction, create SIZE nodes at once and retrieve them from a node "pool"
  *
  **/
 
@@ -28,9 +28,11 @@ private:
         size_t _size;
     public:
         T head;
-        doubly_linked_list() : head(nullptr), _size(0) {};
-        ~doubly_linked_list() {};
+        
+        doubly_linked_list() { init(); };
+        
         void init() { head = nullptr, _size = 0; }
+        
         void insert_node(T prev, T node) {
             if (prev != nullptr) {
                 node->left = prev;
@@ -40,6 +42,7 @@ private:
             }
             ++_size;
         }
+        
         void extract_node(T node) {     // remove node from list leaving untouched its "left" and "right" pointer
             if (_size == 1) head = nullptr;
             else if (node == head) head = node->right;
@@ -47,37 +50,41 @@ private:
             node->left->right = node->right;
             --_size;
         }
+        
         void remove_node(T node) {          // remove node from list and clear its pointer
             extract_node(node);
             node->left = node;
             node->right = node;
         }
+        
         void push_back(T node) {            // append the node at the tail of the list
             if (empty()) insert_node(head = node, node);
             else insert_node(head->left, node);
         }
+        
         bool empty() { return !_size; }
+        
         int size() { return _size; }
+        
         void clear(T &x) { head = nullptr, _size = 0, x = nullptr; }
     };
 
-    template <typename KEYnode, typename DATAnode>
+	template <typename KEY_NODE, typename DATA_NODE>
     class fibonacci_node {
     public:
         // attributes
-        fibonacci_node<KEYnode, DATAnode> *p;
-        doubly_linked_list<fibonacci_node<KEYnode, DATAnode> *> child_list;
-        fibonacci_node<KEYnode, DATAnode> *left;
-        fibonacci_node<KEYnode, DATAnode> *right;
+        fibonacci_node<KEY_NODE, DATA_NODE> *p, *left, *right;
+        doubly_linked_list<fibonacci_node<KEY_NODE, DATA_NODE> *> child_list;
         int degree;
         bool mark;
-        KEY key;
-        DATA data;
+        KEY_NODE key;
+        DATA_NODE data;
+
         //methods
-        fibonacci_node() {};
-        fibonacci_node(KEYnode k, DATAnode d) : p(nullptr), left(this), right(this), degree(0), mark(false), key(k), data(d), child_list() {}
+        fibonacci_node(KEY_NODE k, DATA_NODE d) : p(nullptr), left(this), right(this), degree(0), mark(false), key(k), data(d), child_list() {}
+        
         fibonacci_node(fibonacci_node *node) { if(node != nullptr) key = node->key, data = node->data; }
-        ~fibonacci_node() {};
+        
         bool operator< (const fibonacci_node x){ return key < x.key; }
         bool operator> (const fibonacci_node x){ return key > x.key; }
     };
@@ -117,6 +124,7 @@ private:
             }
         }
     }
+    
     void cut(fibonacci_node<KEY, DATA> *x, fibonacci_node<KEY, DATA> *y) {
         y->child_list.remove_node(x);
         --y->degree;
@@ -124,6 +132,7 @@ private:
         x->p = nullptr;
         x->mark = false;
     }
+    
     void cascading_cut(fibonacci_node<KEY, DATA> *y) {
         fibonacci_node<KEY, DATA> *z = y->p;
         while(z != nullptr){
@@ -136,6 +145,7 @@ private:
             }
         }
     }
+    
     void make_child(fibonacci_node<KEY, DATA> *y, fibonacci_node<KEY, DATA> *x) {
         root_list.remove_node(y);
         x->child_list.push_back(y);
@@ -143,6 +153,7 @@ private:
         y->p = x;
         y->mark = false;
     }
+    
     void insert(fibonacci_node<KEY, DATA> *node) {
         addresses[node->data] = node;
         root_list.push_back(node);
@@ -152,21 +163,29 @@ private:
             min_node = node;
         ++nodes;
     }
+    
     // upper_bound of number of root nodes in the root lists that will be present after consolidation
     int max_degree() { return (int)floor(log((double)nodes)/log((1.0+sqrt(5.0))/2.0))+1; }
+
 public:
     fibonacci_heap() : nodes(0), min_node(nullptr), addresses() {}
+    
     fibonacci_heap(int size, KEY default_key) : nodes(0), min_node(nullptr), addresses() { fill(size, default_key); }
-    ~fibonacci_heap() {};
+    
     void fill(int size, KEY default_key){
         for(int node_id=0; node_id<size; ++node_id)
             insert(new fibonacci_node<KEY, DATA>(default_key, node_id));
     }
+    
     bool empty() { return !nodes; }
+    
     void insert(KEY k, DATA d) { insert(new fibonacci_node<KEY, DATA>(k, d)); }
+    
     std::pair<KEY, DATA> extract_min(){
+    	std::pair<KEY, DATA> x;
         extracted = min_node;
         if (extracted != nullptr) {
+        	x = {extracted->key, extracted->data};
             while (extracted->child_list.size()) {
                 child = extracted->child_list.head->right;
                 extracted->child_list.remove_node(child);
@@ -182,11 +201,11 @@ public:
             }
             --nodes;
             addresses.erase(extracted->data);
+            delete extracted;	
         }
-        std::pair<KEY, DATA> x({extracted->key, extracted->data});
-        delete extracted;
         return x;
     }
+    
     void decrease_key(KEY key, DATA data) {
         if(addresses.count(data) == 0) return;
         fibonacci_node<KEY, DATA> *node = addresses[data];
@@ -201,12 +220,13 @@ public:
                 min_node = node;
         }
     }
+    
     int size() { return nodes; }
 };
 
 
 int main(int argc, char const *argv[]) {
-    
+
     fibonacci_heap<int, std::string> FH({});
 
     return 0;
