fdbdb9a1b4eee47b6b5dca6e8f84c34585053f8a
fedemengo <mengozzi.federico@gmail.com>
2018-08-10 14:34:10 +0200

    Update tests

--- data-structures/heap/binary_heap/test.cpp
+++ data-structures/heap/binary_heap/test.cpp
@@ -1,25 +1,74 @@
-#include "heap.hpp"
+#include "binary_heap.hpp"
+#include <exception>
+
+void test_pop(heap<int, std::string> &h, int k, std::string v) {
+	auto x = h.top();
+	h.pop();
+
+	if(x.first != k){
+		throw std::invalid_argument("Expected key " + std::to_string(k) + " but got " + std::to_string(x.first));
+	}
+	if(x.second != v){
+		throw std::invalid_argument("Expected value " + v + " but got " + x.second);
+	}
+}
 
 int main(int argc, char const *argv[]) {
 
-    heap<int, std::string> H([](int k1, int k2){ return k1 > k2;});
+	heap<int, std::string> max_heap([](int k1, int k2){ return k1 > k2;});
+	heap<int, std::string> min_heap([](int k1, int k2){ return k1 < k2;});
 
-	H.push(0, "awesome");
-    H.push(1, "hello");
-	H.push(2, "dope");
-	H.push(4, "wow");
-	H.push(10, "awdacce");
-    H.push(11, "h12dd");
-	H.push(12, "fferras");
-	H.push(14, "norrerg");
+	try {
+		max_heap.push(0, "a");	
+		max_heap.push(1, "z");
+		max_heap.push(2, "x");
+		max_heap.push(3, "w");
+		max_heap.push(5, "c");
+		max_heap.push(7, "h");
+		max_heap.push(11, "f");
+		max_heap.push(13, "n");
 
-	H.update_key(13, "dope");
+		min_heap.push(0, "a");	
+		min_heap.push(1, "z");
+		min_heap.push(2, "x");
+		min_heap.push(3, "w");
+		min_heap.push(5, "c");
+		min_heap.push(7, "h");
+		min_heap.push(11, "f");
+		min_heap.push(13, "n");
 
-	while(H.size()) {
-		auto x = H.top(); H.pop();
-		std::cout << x.second << std::endl;
+		max_heap.update_key(12, "w");
+		max_heap.update_key(8, "a");
+		max_heap.update_key(9, "x");
+
+		min_heap.update_key(-3, "w");
+		min_heap.update_key(4, "f");
+		min_heap.update_key(-1, "z");
+
+		test_pop(max_heap, 13, "n");	
+		test_pop(max_heap, 12, "w");
+		test_pop(max_heap, 11, "f");
+		test_pop(max_heap, 9, "x");
+		test_pop(max_heap, 8, "a");
+		test_pop(max_heap, 7, "h");
+		test_pop(max_heap, 5, "c");
+		test_pop(max_heap, 1, "z");
+			
+		test_pop(min_heap, -3, "w");	
+		test_pop(min_heap, -1, "z");
+		test_pop(min_heap, 0, "a");
+		test_pop(min_heap, 2, "x");
+		test_pop(min_heap, 4, "f");
+		test_pop(min_heap, 5, "c");
+		test_pop(min_heap, 7, "h");
+		test_pop(min_heap, 13, "n");
+
+		std::cout << "OK" << std::endl;
+	} catch (std::exception &e) {
+		std::cout << "ERROR" << std::endl;
+		std::cout << e.what() << std::endl;
 	}
-	
+
 	return 0;
 }
 
--- data-structures/heap/fibonacci_heap/test.cpp
+++ data-structures/heap/fibonacci_heap/test.cpp
@@ -1,19 +1,60 @@
-#include "fibo.hpp"
+#include "fibonacci_heap.hpp"
+#include <exception>
+
+void test_extract(fibonacci_heap<int, std::string> &h, int k, std::string v) {
+	auto x = h.extract_min();
+
+	if(x.first != k){
+		throw std::invalid_argument("Expected key " + std::to_string(k) + " but got " + std::to_string(x.first));
+	}
+	if(x.second != v){
+		throw std::invalid_argument("Expected value " + v + " but got " + x.second);
+	}
+}
 
 int main(int argc, char const *argv[]) {
 
-    fibonacci_heap<int, std::string> F1({}), F2({});
+    fibonacci_heap<int, std::string> F1({}), F2({}), F3({});
 
-    F1.insert(1, "hello");
-	F2.insert(2, "dope");
-	F1.insert(4, "wow");
-	F2.insert(0, "awesome");
+	try {
+		F1.insert(1, "hello");
+		F2.insert(2, "dope");
+		F1.insert(4, "wow");
+		F2.insert(0, "awesome");
 
-	F1.merge(F2);
+		F3.insert(0, "a");	
+		F3.insert(11, "z");
+		F3.insert(2, "x");
+		F3.insert(3, "w");
+		F3.insert(5, "c");
+		F3.insert(7, "h");
+		F3.insert(11, "f");
+		F3.insert(13, "n");
 
-	while(F1.size()) {
-		auto x = F1.extract_min();
-		std::cout << x.second << std::endl;
+		F1.merge(F2);
+
+		test_extract(F1, 0, "awesome");
+		test_extract(F1, 1, "hello");
+		test_extract(F1, 2, "dope");
+		test_extract(F1, 4, "wow");
+
+		F3.decrease_key(-3, "w");
+		F3.decrease_key(4, "f");
+		F3.decrease_key(-1, "z");
+
+		test_extract(F3, -3, "w");
+		test_extract(F3, -1, "z");
+		test_extract(F3, 0, "a");
+		test_extract(F3, 2, "x");
+		test_extract(F3, 4, "f");
+		test_extract(F3, 5, "c");
+		test_extract(F3, 7, "h");
+		test_extract(F3, 13, "n");
+
+		std::cout << "OK" << std::endl;
+	} catch (std::exception &e) {
+		std::cout << "ERROR" << std::endl;
+		std::cout << e.what() << std::endl;
 	}
 	
 	return 0;
