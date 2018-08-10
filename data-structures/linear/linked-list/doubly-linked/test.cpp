2e80d285b5b1185365261adfb47d314e08eac174
fedemengo <mengozzi.federico@gmail.com>
2018-08-10 17:17:02 +0200

    Improve tests for linked lists

--- data-structures/linear/linked-list/doubly-linked/test.cpp
+++ data-structures/linear/linked-list/doubly-linked/test.cpp
@@ -1,5 +1,18 @@
-#include <bits/stdc++.h>
 #include "doubly_linked.hpp"
+#include <vector>
+#include <exception>
+
+void test_at(doubly_linked<std::string> &l, int index, std::string v) {
+	if(l[index] != v){
+		throw std::invalid_argument("Expected value " + v + " but got " + l[index]);
+	}
+}
+
+void test_equal(std::string u, std::string v) {
+	if(u != v){
+		throw std::invalid_argument("Expected value " + v + " but got " + u);
+	}
+}
 
 void print(doubly_linked<std::string> &l){
     for(auto &x : l){
@@ -9,32 +22,52 @@ void print(doubly_linked<std::string> &l){
 }
 
 int main(int argc, char const *argv[]) {
-    int N = atoi(argv[1]);
+
     srand(time(0));
 
     doubly_linked<std::string> list({});
+    std::vector<std::string> check;
 
-    for(int i=0; i<N; ++i){
-        std::string str = "h";
-        str.append(std::to_string(i));
-        list.insert(rand() % (list.size() + 1), str);
-        list.print();
-    }
-    
-    //for(std::string str : list){
-    //    std::cout << str << std::endl;
-    //}
+    try {
+        for(int i=0; i<1000; ++i){
+            std::string str = "h";
+            str.append(std::to_string(i));
+            int index = rand() % (list.size() + 1);
+            list.insert(index, str);
+            check.insert(check.begin() + index, str);
+        }
+        
+        for(int i=0; i<check.size(); i++) {
+            test_at(list, i, check[i]);
+        }
 
-    //for(auto it = list.begin(); it != list.end(); it++){
-    //    std::cout << *it << std::endl;
-    //}
+        int i = 0;
+        for(std::string &str: list){
+            test_equal(str, check[i]);
+            ++i;
+        }
 
-    while(list.size()){
-        list.remove(rand() % list.size());
-        //print(list);
-        //list.pop_back();
-        list.print();
-    }
+        i = 0;
+        for(auto it = list.begin(); it != list.end(); i++, it++){
+            test_equal(*it, check[i]);
+        }
+
+        while(list.size()){
+            int index = rand() % list.size();
+            list.remove(index);
+            check.erase(check.begin() + index);
+            
+            for(int i=0; i<list.size(); ++i){
+                test_at(list, i, check[i]);
+            }
+        }
+
+        std::cout << "OK" << std::endl;
+
+    } catch (std::exception &e) {
+		std::cout << "ERROR" << std::endl;
+		std::cout << e.what() << std::endl;
+	}
 
     return 0;
 }
--- data-structures/linear/linked-list/single-linked/test.cpp
+++ data-structures/linear/linked-list/single-linked/test.cpp
@@ -1,34 +1,73 @@
-#include <bits/stdc++.h>
 #include "single_linked.hpp"
+#include <vector>
+#include <exception>
+
+void test_at(single_linked<std::string> &l, int index, std::string v) {
+	if(l[index] != v){
+		throw std::invalid_argument("Expected value " + v + " but got " + l[index]);
+	}
+}
+
+void test_equal(std::string u, std::string v) {
+	if(u != v){
+		throw std::invalid_argument("Expected value " + v + " but got " + u);
+	}
+}
 
 void print(single_linked<std::string> &l){
     for(auto &x : l){
-        std::cout << std::setw(2) << x << " ";
+        std::cout << x << " ";
     }
     std::cout << std::endl;
 }
 
 int main(int argc, char const *argv[]) {
-    int N = atoi(argv[1]);
+
     srand(time(0));
 
     single_linked<std::string> list({});
+    std::vector<std::string> check;
 
-    for(int i=0; i<N; ++i){
-        std::string str = "h";
-        str.append(std::to_string(i));
-        list.insert(rand() % (list.size() + 1), str);
-        //list.push_back(str);
-        list.print();
-    }
+    try {
+        for(int i=0; i<1000; ++i){
+            std::string str = "h";
+            str.append(std::to_string(i));
+            int index = rand() % (list.size() + 1);
+            list.insert(index, str);
+            check.insert(check.begin() + index, str);
+        }
+        
+        for(int i=0; i<check.size(); i++) {
+            test_at(list, i, check[i]);
+        }
 
+        int i = 0;
+        for(std::string &str: list){
+            test_equal(str, check[i]);
+            ++i;
+        }
 
-    while(list.size()){
-        list.remove(rand() % list.size());
-        //list.pop_back();
-        //print(list);
-        list.print();
-    }
+        i = 0;
+        for(auto it = list.begin(); it != list.end(); i++, it++){
+            test_equal(*it, check[i]);
+        }
+
+        while(list.size()){
+            int index = rand() % list.size();
+            list.remove(index);
+            check.erase(check.begin() + index);
+            
+            for(int i=0; i<list.size(); ++i){
+                test_at(list, i, check[i]);
+            }
+        }
+
+        std::cout << "OK" << std::endl;
+
+    } catch (std::exception &e) {
+		std::cout << "ERROR" << std::endl;
+		std::cout << e.what() << std::endl;
+	}
 
     return 0;
 }
