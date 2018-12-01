e13aa584b60ab8749685c7509475a4692ab5ee46
fedemengo <mengozzi.federico@gmail.com>
2018-11-30 23:53:41 -0800

    Clean up

--- data-structures/linear/minmax_queue/minmax_queue.hpp
+++ data-structures/linear/minmax_queue/minmax_queue.hpp
@@ -4,42 +4,37 @@
 template <typename T>
 class minmax_queue {
 private:
-	std::queue<T> q;
-	std::queue<T> best;
-	std::function<bool(T, T)> compare;
+    std::queue<T> q;
+    std::queue<T> best;
+    std::function<bool(T, T)> compare;
 
 public:
-	minmax_queue(std::function<bool(T, T)> cmp) : compare(cmp) {}
-	minmax_queue() : minmax_queue([](T e1, T e2) { return e1 < e2; }) {}
+    minmax_queue(std::function<bool(T, T)> cmp) : compare(cmp) {}
+    minmax_queue() : minmax_queue([](T e1, T e2) { return e1 < e2; }) {}
 
-	void push(T e) {
-		q.push(e);
-		if(best.size() == 0) {
-			best.push(e);
-		} else if(compare(e, best.front())) {
-			while(best.size()) best.pop();
-			best.push(e);
-		} else {
-			best.push(e);
-		}
-	}
-	
-	T front() {
-		return q.front();
-	}
+    void push(T e) {
+        q.push(e);
+        if(best.size() != 0 && compare(e, best.front())) {
+            best = std::queue<T>();
+            //while(best.size()) best.pop();
+        }
+        best.push(e);
+    }
+    
+    T front() {
+        return q.front();
+    }
 
-	T minmax() {
-		return best.front();
-	}
+    T minmax() {
+        return best.front();
+    }
 
-	void pop(){
-		if(q.front() == best.front()) {
-			q.pop();
-			best.pop();
-		} else {
-			q.pop();
-		}
-	}
+    void pop(){
+        if(q.front() == best.front()) {
+            best.pop();
+        }
+        q.pop();
+    }
 
-	ssize_t size() { return q.size(); }
+    ssize_t size() { return q.size(); }
 };
